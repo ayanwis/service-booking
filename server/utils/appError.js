e22b20ee64cf06b3d75783e00dd201073e9590dc
kartick17 <karticksadhu0@gmail.com>
2023-12-02 13:56:51 +0530

    Fix error handling

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,13 +1,12 @@
 class AppError extends Error {
   constructor(message, statusCode) {
-    super(message);
+    super(message)
 
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
-    this.isOperational = true;
+    this.statusCode = statusCode
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
 
-    Error.captureStackTrace(this, this.constructor);
+    Error.captureStackTrace(this, this.constructor)
   }
 }
 
-module.exports = AppError;
+module.exports = AppError
