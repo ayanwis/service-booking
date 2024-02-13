caf1060811be7e4d88655a7ccab9989acfbcd246
ayanonline <ayanghosh1389@gmail.com>
2024-02-13 11:47:05 +0530

    changed token data

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,15 +5,19 @@ const jwt = require("jsonwebtoken");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-const jwtSign = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-    // expiresIn: 100   //100s
-  });
+const jwtSign = (user) =>
+  jwt.sign(
+    { id: user._id, name: user.name, email: user.email, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+      // expiresIn: 100   //100s
+    }
+  );
 
 const createSendToken = (user, statusCode, res) => {
   // Create JWT token
-  const token = jwtSign(user._id);
+  const token = jwtSign(user);
   const cookieOptions = {
     expires: new Date(Date.now() + 31 * 24 * 60 * 60 * 1000),
     httpOnly: true,
